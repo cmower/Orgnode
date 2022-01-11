31c12d07a3eb0b2d162314d1431f2455c1d468b6
Chris E. Mower <chris.mower@ed.ac.uk>
2022-01-11 18:47:11 +0000

    Package'ify Orgnode

---
+++ Orgnode/__init__.py
@@ -0,0 +1,1 @@
+from .orgnode import *
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='Orgnode',
+    version='1.0',
+    description='Mirror of Orgnode.',
+    url='https://github.com/cmower/Orgnode',
+    author='Charles Cave',
+    author_email='charlesweb@optusnet.com.au',
+    maintainer='Christopher E. Mower',
+    maintainer_email='c.mower@ed.ac.uk',
+    license='MIT License',
+    packages=['Orgnode'],
+    zip_safe=False
+)
